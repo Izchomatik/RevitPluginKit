e7a6d0e4fb305cd207f04e1ba370b04d0197de33
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2021-12-14 18:59:05 +0300

    StringConverter add

---
+++ RevitPluginKit/src/converters/StringConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace RevitPluginKit.Converters
+{
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Class for converting values from metric to imperial and vice versa.
+    /// </summary>
+    public class StringConverter
+    {
+        /// <summary>
+        /// Convert stopwatch to string.
+        /// </summary>
+        /// <param name="stopwatch"> System stopwatch value. </param>
+        /// <returns>
+        /// Return formatted string.
+        /// </returns>
+        public static string StopwatchToString(Stopwatch stopwatch)
+        {
+            TimeSpan timespan = stopwatch.Elapsed;
+            return string.Format(
+                format: "{0:00}:{1:00}:{2:00}.{3:00}",
+                timespan.Hours,
+                timespan.Minutes,
+                timespan.Seconds,
+                timespan.Milliseconds / 10);
+        }
+    }
+}
