c3255b39ca58f2f584c4833bc1d399b0697c2444
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2021-12-14 17:19:36 +0300

    ImageServices add

---
+++ RevitPluginKit/src/ui/ImageServices.cs
@@ -0,0 +1,35 @@
+﻿namespace RevitPluginKit.Ui
+{
+    using System.IO;
+    using System.Reflection;
+    using System.Windows.Media.Imaging;
+
+    /// <summary>
+    /// Services for working with images.
+    /// </summary>
+    internal class ImageServices
+    {
+        /// <summary>
+        /// Image extractor. Read specific embed image using address string.
+        /// </summary>
+        /// <param name="assembly"> Current assembly. </param>
+        /// <param name="address"> The address in the solution where the image is located. </param>
+        /// <returns>
+        /// Return BitmapSource instance.
+        /// </returns>
+        internal static BitmapSource GetEmbeddedImage(
+            Assembly assembly,
+            string address)
+        {
+            try
+            {
+                Stream stream = assembly.GetManifestResourceStream(name: address);
+                return BitmapFrame.Create(stream);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
