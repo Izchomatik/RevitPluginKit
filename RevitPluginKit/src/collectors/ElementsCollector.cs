82303e2429cdef05e496048ae54c2bf39eb90da7
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2021-12-17 17:50:05 +0300

    collectors init

---
+++ RevitPluginKit/src/collectors/ElementsCollector.cs
@@ -0,0 +1,50 @@
+﻿namespace RevitPluginKit.Collectors
+{
+    using System.Collections.Generic;
+    using Autodesk.Revit.DB;
+    using static RevitPluginKit.Collectors.FilterUtilities;
+
+    /// <summary>
+    /// A class containing methods for quickly finding elements in the Revit model.
+    /// </summary>
+    public class ElementsCollector
+    {
+        /// <summary>
+        /// Collect Revit element instances by category.
+        /// </summary>
+        /// <param name="document"> Current revit document instance. </param>
+        /// <param name="category"> Revit BuiltIn category to collect. </param>
+        /// <param name="familyName"> Optional parameter: if necessary, specify the name of the family of the elements to be collected. </param>
+        /// <param name="typeName"> Optional parameter: if necessary, specify the type name of the elements to be collected. </param>
+        /// <param name="useOptionFilter"> Optional parameter: indicate whether it is necessary to use an option filter. </param>
+        /// <param name="optionFilter"> Optional parameter: specify option filter.
+        /// If useOptionFilter == true and optionFilter == null => items from the currently active option will be collected. </param>
+        /// <returns>
+        /// Returns the list of Revit elements (Element).
+        /// </returns>
+        public static List<Element> InstancesByCategory(
+            Document document,
+            BuiltInCategory category,
+            string familyName = null,
+            string typeName = null,
+            bool useOptionFilter = true,
+            ElementDesignOptionFilter optionFilter = null)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(document)
+                .WhereElementIsNotElementType()
+                .OfCategory(category);
+            if (useOptionFilter == true)
+            {
+                ElementDesignOptionFilter checkedOptionFilter = CheckOptionFilter(
+                    document: document,
+                    optionFilter: optionFilter);
+                collector = collector.WherePasses(checkedOptionFilter);
+            }
+
+            return FilterByFamilyAndType(
+                collector: collector,
+                familyName: familyName,
+                typeName: typeName);
+        }
+    }
+}
---
+++ RevitPluginKit/src/collectors/FilterUtilities.cs
@@ -0,0 +1,76 @@
+﻿namespace RevitPluginKit.Collectors
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Autodesk.Revit.DB;
+
+    /// <summary>
+    /// A set of internal methods needed to work with filters.
+    /// </summary>
+    internal class FilterUtilities
+    {
+        /// <summary>
+        /// Method for verifying the incoming option filter.
+        /// </summary>
+        /// <param name="document"> Current revit document instance. </param>
+        /// <param name="optionFilter"> Incoming option filter to be checked. </param>
+        /// <returns>
+        /// Checked option filter.
+        /// </returns>
+        internal static ElementDesignOptionFilter CheckOptionFilter(Document document, ElementDesignOptionFilter optionFilter)
+        {
+            if (optionFilter == null)
+            {
+                ElementId activeOptionID = DesignOption.GetActiveDesignOptionId(document);
+                optionFilter = new ElementDesignOptionFilter(activeOptionID);
+            }
+
+            return optionFilter;
+        }
+
+        /// <summary>
+        /// Filter elements by family name and/or type name.
+        /// </summary>
+        /// <param name="collector"> Revit API FilteredElementCollector. </param>
+        /// <param name="familyName"> Name of the family of the elements to be collected. </param>
+        /// <param name="typeName"> Type name of the elements to be collected. </param>
+        /// <returns>
+        /// Returns the list of Revit elements (Element).
+        /// </returns>
+        internal static List<Element> FilterByFamilyAndType(
+            FilteredElementCollector collector,
+            string familyName,
+            string typeName)
+        {
+            if (familyName != null && typeName != null)
+            {
+                return collector
+                    .Cast<FamilyInstance>()
+                    .Where(i => i.Symbol.Family.Name.Equals(familyName) && i.Name.Equals(typeName))
+                    .Cast<Element>()
+                    .ToList();
+            }
+            else if (familyName != null)
+            {
+                return collector
+                    .Cast<FamilyInstance>()
+                    .Where(i => i.Symbol.Family.Name.Equals(familyName))
+                    .Cast<Element>()
+                    .ToList();
+            }
+            else if (typeName != null)
+            {
+                return collector
+                    .Where(i => i.Name.Equals(typeName))
+                    .Cast<Element>()
+                    .ToList();
+            }
+            else
+            {
+                return collector
+                    .Cast<Element>()
+                    .ToList();
+            }
+        }
+    }
+}
