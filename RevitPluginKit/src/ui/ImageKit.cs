54953c6bdfc6b5f36b2ffae385b0e0f0973e6d06
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2021-12-23 19:09:52 +0300

    Update ImageKit.cs

--- RevitPluginKit/src/ui/ImageKit.cs
+++ RevitPluginKit/src/ui/ImageKit.cs
@@ -1,5 +1,6 @@
 ﻿namespace RevitPluginKit.Ui
 {
+    using System;
     using System.IO;
     using System.Reflection;
     using System.Windows.Media.Imaging;
@@ -10,24 +11,35 @@
     public class ImageKit
     {
         /// <summary>
+        /// Image extractor. Read specific resource image using address string.
+        /// </summary>
+        /// <param name="uriString"> The Uri address in the solution where the image is located. </param>
+        /// <returns>
+        /// Return BitmapImage instance.
+        /// </returns>
+        public static BitmapImage GetResourceImage(string uriString)
+        {
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(uriString: uriString);
+            image.EndInit();
+            return image;
+        }
+
+        /// <summary>
         /// Image extractor. Read specific embed image using address string.
         /// </summary>
-        /// <param name="address"> The address in the solution where the image is located. </param>
         /// <param name="assembly"> Current assembly. </param>
+        /// <param name="address"> The address in the solution where the image is located. </param>
         /// <returns>
         /// Return BitmapSource instance.
         /// </returns>
-        public static BitmapSource GetEmbeddedImage(
-            string address,
-            Assembly assembly = null)
+        internal static BitmapSource GetEmbeddedImage(
+            Assembly assembly,
+            string address)
         {
             try
             {
-                if (assembly == null)
-                {
-                    assembly = Assembly.GetCallingAssembly();
-                }
-
                 Stream stream = assembly.GetManifestResourceStream(name: address);
                 return BitmapFrame.Create(stream);
             }
