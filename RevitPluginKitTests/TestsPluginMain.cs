aed206022df459967ab10a1330fcbf5bf8051ec9
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2022-01-12 11:46:10 +0300

    main add

---
+++ RevitPluginKitTests/TestsPluginMain.cs
@@ -0,0 +1,59 @@
+﻿namespace RevitPluginKitTests
+{
+    using System.Collections.Generic;
+    using Autodesk.Revit.UI;
+    using RevitPluginKit.Ui;
+    using static RevitPluginKit.Ui.RibbonKit;
+
+    /// <summary>
+    /// Main test plug-in entry point.
+    /// </summary>
+    public class TestsPluginMain : IExternalApplication
+    {
+        /// <inheritdoc/>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        /// <inheritdoc/>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            AddRibbonTab(
+                application: application,
+                tabName: "RPK Tests",
+                panelsSettings: new List<RibbonPanelSettings>()
+                {
+                    new RibbonPanelSettings(
+                        name: "Major tests",
+                        buttonsSettings: new List<ButtonSettings>()
+                        {
+                            new PushButtonSettings(
+                                internalName: "generalTest",
+                                name: "General\ntest",
+                                tooltip: "Runs all RevitPluginKit tests.",
+                                imageAddress: "RevitPluginKitTests.assets.icons.GeneralTestIcon.png",
+                                className: "RevitPluginKitTests.Src.GeneralTest"),
+                        }),
+                    new RibbonPanelSettings(
+                        name: "Minor tests",
+                        buttonsSettings: new List<ButtonSettings>()
+                        {
+                            new PushButtonSettings(
+                                internalName: "collectorsTest",
+                                name: "Collectors\ntest",
+                                tooltip: "Runs multiple checks related to collecting items in the model using RevitPluginKit",
+                                imageAddress: "RevitPluginKitTests.assets.icons.CollectorsTestIcon.png",
+                                className: "RevitPluginKitTests.Src.CollectorsTest"),
+                            new PushButtonSettings(
+                                internalName: "convertersTest",
+                                name: "Converters\ntest",
+                                tooltip: "Runs multiple checks related to value conversions using RevitPluginKit",
+                                imageAddress: "RevitPluginKitTests.assets.icons.ConvertersTestIcon.png",
+                                className: "RevitPluginKitTests.Src.ConvertersTest"),
+                        }),
+                });
+            return Result.Succeeded;
+        }
+    }
+}
