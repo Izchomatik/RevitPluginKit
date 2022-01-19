1b89437bec73a1dbbab5ab77fc7d7afad676f0a3
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2022-01-19 18:51:30 +0300

    doc update

--- RevitPluginKit/src/collectors/ElementsCollector.cs
+++ RevitPluginKit/src/collectors/ElementsCollector.cs
@@ -18,7 +18,7 @@
         /// It is possible to pass any document loaded to the current model (to search for elements in the specified document).</param>
         /// <param name="category">
         /// Revit BuiltIn category to collect.
-        /// Example: <code>BuiltInCategory.OST_Walls</code> </param>
+        /// Example: <c>BuiltInCategory.OST_Walls</c>.</param>
         /// <param name="familyName"> Optional parameter: if necessary, specify the name of the family of the elements to be collected. </param>
         /// <param name="typeName"> Optional parameter: if necessary, specify the type name of the elements to be collected. </param>
         /// <param name="levelIdsToFilterBy"> Optional parameter: if necessary, specify list of level ids of the elements to be collected. </param>
