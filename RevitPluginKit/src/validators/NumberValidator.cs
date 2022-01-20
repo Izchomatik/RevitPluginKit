d7b431976a6fe552839c123ae325e046199e0ace
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2022-01-20 14:16:11 +0300

    NumberValidation docs update

--- RevitPluginKit/src/validators/NumberValidator.cs
+++ RevitPluginKit/src/validators/NumberValidator.cs
@@ -5,14 +5,17 @@
 
     /// <summary>
     /// Number validator class.
+    /// <para>Designed to check incoming numeric values. Mainly used to process incoming user data.</para>
     /// </summary>
     public class NumberValidator
     {
         /// <summary>
         /// Number validation method for wpf textBox.
+        /// <para>Accepts user input and returns a boolean value indicating whether the input matches a numeric value.</para>
+        /// <para>This method is designed to be used when processing data coming through the WPF (System.Windows.Input.TextCompositionEventArgs).</para>
         /// </summary>
-        /// <param name="sender"> Sender object. </param>
-        /// <param name="e"> Text composition event. </param>
+        /// <param name="sender"> WPF sender object. </param>
+        /// <param name="e"> WPF text composition event. </param>
         public static void NumberValidation(
             object sender,
             TextCompositionEventArgs e)
