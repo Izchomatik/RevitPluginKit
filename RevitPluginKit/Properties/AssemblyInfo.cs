c78f25efd1e4eb9eda54db32de5108c98c49bd06
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2022-01-28 15:18:53 +0300

    assembly onfo update

--- RevitPluginKit/Properties/AssemblyInfo.cs
+++ RevitPluginKit/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RevitPluginKit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library with a set of tools for preparing Revit plug-in based on .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("1")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RevitPluginKit")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyCopyright("Copyright © 2022 Ilia Zhukov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
