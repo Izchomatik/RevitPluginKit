157e162299fcda4bb2f7148e4044896f70c3a9ef
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2022-02-09 19:16:08 +0300

    ObservableItemUtilities add

---
+++ RevitPluginKit/src/wpf/ObservableItemUtilities.cs
@@ -0,0 +1,32 @@
+﻿namespace RevitPluginKit.Wpf
+{
+    using System.Collections.Generic;
+    using System.Windows.Controls;
+
+    class ObservableItemUtilities<T>
+    {
+        public static void ChangeAllItems(
+            List<ObservableItem<T>> observableItems,
+            bool requiredValue)
+        {
+            foreach (ObservableItem<T> observableItem in observableItems)
+            {
+                observableItem.IsChecked = requiredValue;
+            }
+        }
+
+        public static void SelectionClick(object sender)
+        {
+            ListBox itemList = sender as ListBox;
+            var selectedItems = itemList.SelectedItems;
+            if (selectedItems != null && selectedItems.Count > 0)
+            {
+                foreach (var item in selectedItems)
+                {
+                    ObservableItem<T> observableItem = item as ObservableItem<T>;
+                    observableItem.IsChecked = !observableItem.IsChecked;
+                }
+            }
+        }
+    }
+}
