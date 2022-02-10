39b033e4f2bcbdff9741e566ef8b3c4bddceda45
i.zhukov <58937357+Izchomatik@users.noreply.github.com>
2022-02-10 14:57:57 +0300

    wpf test win add

---
+++ RevitPluginKit/articles/wpfTools.md
@@ -0,0 +1,20 @@
+
+# WPF tools
+
+## Validator tools
+
+A set of tools designed to validate values.
+
+* [Validators Api documentation](https://izchomatik.github.io/RevitPluginKit/api/RevitPluginKit.Validators.html)
+
+* Namespace: `RevitPluginKit.Validators`
+
+	* WPF validation of an input value as being a number is supported.
+
+> [!NOTE]
+>
+> The most convenient way to use validators - is with a using declaration:
+>
+> ```csharp
+> using RevitPluginKit.Validators;
+> ```
---
+++ RevitPluginKitTests/src/wins/WPFTestViewer.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace RevitPluginKitTests.Wins
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Main results report window.
+    /// </summary>
+    public partial class WPFTestViewer : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WPFTestViewer"/> class.
+        /// </summary>
+        public WPFTestViewer()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
